4460b0b7db88fd219a73bbd57b18d6f909d42c49
Vicente M. G. H <vicman.lobo@gmail.com>
2014-12-07 00:33:12 +0100

    Player con Mechanim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public Vector3 speed;
+	private Animator animController;							
+	public float animSpeed = 1.5f;				
+	public Vector3 lookTarget;
+	
+	void Start () {
+		animController = GetComponent<Animator>();					  
+	}
 
 	void FixedUpdate () {
-		rigidbody.AddForce(speed);
+		float h = Input.GetAxis("Horizontal");				// setup h variable as our horizontal input axis
+		animController.SetFloat("Speed", 1);							// set our animator's float parameter 'Speed' equal to the vertical input axis				
+		animController.speed = animSpeed;								// set the speed of our animator to the public variable 'animSpeed'
 	}
 }
