c79676c391c0e58a6c09298d32136fea9a1da1fb
Vicente M. G. H <vicman.lobo@gmail.com>
2014-12-07 19:34:21 +0100

    GameDirector ahora sabe el Level actual del Player

--- Assets/Scripts/blocksScripts/BlockEndExitController.cs
+++ Assets/Scripts/blocksScripts/BlockEndExitController.cs
@@ -5,12 +5,16 @@ public class BlockEndExitController : MonoBehaviour {
 	public Vector3 nextPosition;
 	public bool autoGenerateMap = false;
 	public GameDirector gameDirector;
+	public int mapIndex;
 	
 	void OnTriggerEnter(Collider other) {
 		
 		if (autoGenerateMap == true) {
 			nextPosition = gameDirector.AutoGenerateMap();
 		}
+		
+		gameDirector.playerLevel += 1;
+		gameDirector.playerMapLevel = mapIndex+1;
 		
 		other.transform.position = nextPosition;
 	}
