821cd5036110eb1142e1455f6500465827eaf085
Pedro Marín Ramos <pmarinr@gmail.com>
2014-12-07 17:36:52 +0100

    Sirena Prefabs

---
+++ Assets/rotateScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateScript : MonoBehaviour {
+	public float velocity;
+	public Vector3 ejes;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (ejes * velocity * Time.deltaTime);
+	}
+}
