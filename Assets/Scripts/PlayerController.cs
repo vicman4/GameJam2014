039ff620707d8c3aba558f9757a1646425ae1fc2
Vicente M. G. H <vicman.lobo@gmail.com>
2014-12-07 01:42:10 +0100

    Recuperando Map y PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public Vector3 speed;
-	public float maxSpeed = 10f;
+	private Animator animController;							
+	public float animSpeed = 1.5f;				
+	public Vector3 lookTarget;
+	
+	void Start () {
+		animController = GetComponent<Animator>();					  
+	}
 
 	void FixedUpdate () {
-		if (rigidbody.velocity.magnitude < maxSpeed)
-			rigidbody.AddForce(speed,ForceMode.Force);
-
+		float h = Input.GetAxis("Horizontal");				// setup h variable as our horizontal input axis
+		animController.SetFloat("Speed", 1);							// set our animator's float parameter 'Speed' equal to the vertical input axis				
+		animController.speed = animSpeed;								// set the speed of our animator to the public variable 'animSpeed'
 	}
 }
