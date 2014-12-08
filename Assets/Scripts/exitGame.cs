5f43692515fbe6062d97db66cfb05325d18177e3
Pedro Marín Ramos <pmarinr@gmail.com>
2014-12-08 14:35:26 +0100

    Menu mejorado

---
+++ Assets/Scripts/exitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exitGame : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+		if(col.transform.tag == "Player"){
+			Debug.Log("Exit Game");
+			Application.Quit();
+		}
+		
+	}
+}
